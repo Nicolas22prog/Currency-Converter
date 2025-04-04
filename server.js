791f7f11b9484476530efa6e2ea2be3da32c2007
Nicolas22Prog <nicolasmonteiro8@gmail.com>
2025-04-04 10:57:39 -0300

    all files commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+const port = 3000;
+const apiKey = '9e26dbf503a64692956747dfabb296ff'; // Substitua por sua chave real
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.post('/convert', async (req, res) => {
+  const { amount, fromCurrency, toCurrency } = req.body;
+  try {
+    const response = await axios.get(`https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKey}`);
+    const rates = response.data.rates;
+    const exchangeRate = rates[toCurrency];
+    if (exchangeRate) {
+      const convertedAmount = (amount * exchangeRate).toFixed(2);
+      res.json({ amount, fromCurrency, toCurrency, convertedAmount });
+    } else {
+      res.status(400).json({ error: 'Código de moeda inválido' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar taxas de câmbio' });
+  }
+});
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
